6be8b48e738b510a6506caed521350305aeee3ce
Jennifer Minnich <jminniecc@gmail.com>
2017-01-10 15:51:13 -0700

    added pic and sample HTML

--- index.php
+++ index.php
@@ -5,6 +5,45 @@
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
+		<header>
+		<h1>This is a demo page!</h1>
+		</header>
+		<main>
 
+			<p>rikers ipsum</p>
+
+		<h2>A Few of my Favorite Things</h2>
+			<!-- This is a cheeky comment-->
+		<ul>
+			<li>Dogs</li>
+			<li>Tacos</li>
+			<li>Poems</li>
+		</ul>
+
+		<h3>This is a table</h3>
+		<table>
+			<tr> <!--Row 1-->
+				<th>Column 1</th>
+				<th>Column 2</th>
+			</tr>
+
+			<tr><!--Row 2-->
+				<td>Tabby</td>
+				<td>Beagle</td>
+			</tr>
+			<tr><!--Row3-->
+				<td>Persian</td>
+				<td>Bulldog</td>
+			</tr>
+			<tr><!--Row 4-->
+				<td>Siamese</td>
+				<td>Boxer</td>
+			</tr>
+		</table>
+
+			<img src="images/catdog.jpg" alt="cat dog">
+
+			<p>This is a link to <a href="https://google.com" target="_blank">Google</a>.</p>
+		</main>
 	</body>
 </html>
